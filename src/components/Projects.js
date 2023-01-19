441eceb0717846b7498f2c8ff541cd8d0e4f2a60
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2023-01-19 16:29:29 +0100

    correction d'erreur

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -212,7 +212,7 @@ export const Projects = () => {
           </Col>
         </Row>
       </Container>
-      <img className="background-image-right" src={colorSharp2}></img>
+      <img className="background-image-right" src={colorSharp2} alt="background formation et projet"></img>
     </section>
   )
 }
