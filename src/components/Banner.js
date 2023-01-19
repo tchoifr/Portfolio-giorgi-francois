c240823416d52555e7531ef32df014e59bfdcaa2
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2023-01-19 18:06:04 +0100

    correction orthographe

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -15,8 +15,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Bienvenue sur mon Portfolio</span>
-                <h1>{`Salut! Je m'appelle François je suis Développeur Full stack`}</h1>
-                  <p>Je suis en recherche active d'un poste sur les Frameworks Symfony/PHP et React.js. Extrêmement motivé pour atteindre les objectifs et challenges qui me sont fixés, les défis ne me font pas peur. De plus, je suis autonome et m'adapte facilement en remote pour collaborer en équipe. N'hésitez pas à consulter mes projets et à me contacter ! Bonne journée.</p>
+                <h1>{`Je suis Développeur Full stack`}</h1>
+                  <p>En recherche active d'un poste sur les Frameworks Symfony/PHP et React.js. Extrêmement motivé pour atteindre les objectifs et challenges qui me sont fixés, les défis ne me font pas peur. De plus, je suis autonome et m'adapte facilement en remote pour collaborer en équipe. N'hésitez pas à consulter mes projets et à me contacter ! Bonne journée.</p>
                 
               </div>}
             </TrackVisibility>
