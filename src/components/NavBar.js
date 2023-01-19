0424255b29c2d746f07f07ffbf61e865dd5f783f
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2023-01-19 16:36:51 +0100

    correction d'erreur

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -48,8 +48,8 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="Linkedin" /></a>
-                <a href="#"><img src={navIcon2} alt="Facebook" /></a>
+                <a href="https://www.linkedin.com/in/francois-giorgi-developpeur-web/"><img src={navIcon1} alt="Linkedin" /></a>
+                <a href="https://www.facebook.com/francoisdev13"><img src={navIcon2} alt="Facebook" /></a>
                 <a href="CV-Francois-Giorgi.pdf" download ><img src={navIcon3} alt="CV" /></a>
               </div>
               
