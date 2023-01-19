0c41546a88d5eb10ea59e7ccdd76abac5caf878d
François Giorgi <77675039+tchoifr@users.noreply.github.com>
2023-01-19 17:22:02 +0100

    style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/newimg/cv3.png";
@@ -10,16 +9,14 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
         
-          <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
-          </Col>
-          <Col size={12} sm={6} className="text-center text-sm-end">
+         
+          <Col  sm={6} className="text-center text-sm-end  ">
             <div className="social-icon">
               <a href="https://www.linkedin.com/in/francois-giorgi-developpeur-web/"><img src={navIcon1} alt="Linkedin" /></a>
               <a href="https://www.facebook.com/francoisdev13"><img src={navIcon2} alt="Facebook" /></a>
               <a href="CV-Francois-Giorgi.pdf" download ><img src={navIcon3} alt="CV" /></a>
             </div>
-            <p>Copyright 2023</p>
+            <p className="fs-6">©Copyright 2023</p>
           </Col>
         </Row>
       </Container>
